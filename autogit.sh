2cac36735b2dca6d5f9adfa2358348023c1246af
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-09-27 15:07:45 -0300

    Application working successfully

---
+++ autogit.sh
@@ -0,0 +1,6 @@
+git add -A
+git commit -m "Application working successfully"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/Java-Docker-Spring-Test.git
+git push -u origin main
+git status
